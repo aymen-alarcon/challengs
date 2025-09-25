fa6ef8148606a5abf3a7d51fc15e48d783d38435
Aymen Oumaalla <aymenoml2002@gmail.com>
2025-09-25 14:05:07 +0100

    question 3 added to challenge 3

--- challenge3.c
+++ challenge3.c
@@ -26,7 +26,7 @@ int main(void) {
         }
     }
     return 0;
-    */
+    
 
     //second question
     bool digit_seen[10] = {false}; 
@@ -59,5 +59,59 @@ printf("Chiffre : \t");
         
     }
     return 0;
+*/
 
+//third question
+    int mother [10];
+    bool digit_seen[10] = {false}; 
+    int digit_repeated[10][10] = {0}; 
+    int digit; 
+    int count = 0;
+    char choice;
+    int numbers = 0;
+
+
+    printf("Entrez un nombre: "); 
+    scanf("%d", &mother[numbers]);
+        debut:
+        printf("Do you want to continue? (y/n): ");
+        scanf(" %c", &choice);
+    if (choice == 'y'){
+        numbers++;
+        printf("Entrez un nombre: "); 
+        scanf("%ld", &mother[numbers]);
+        goto debut;
+    }
+    else if (choice == 'n')
+    {
+        for (int i = 0; i <= numbers; i++) {
+            int temp = mother[i];
+            while (temp > 0) { 
+                digit = temp % 10;             
+                digit_repeated[i][digit]++;
+                temp /= 10;       
+            }              
+        }
+
+        printf("Chiffre : \t");
+        for (int i = 0; i <= numbers; i++) {
+            printf("%d\t", mother[i]);
+        }
+
+        printf("\nOccurrences : \n");
+        for (int i = 0; i <= numbers; i++) {
+            printf("Pour %d :\n\t", mother[i]);
+            for (int j = 0; j < 10; j++) {
+                printf("%d\t", digit_repeated[i][j]);
+            }
+            printf("\n");
+        printf("\nOccurrences : \t");
+        }
+    }
+    else
+    {
+        printf("what do you even want!!!! it's only y or n ediota\n");
+        goto debut;
+    }
 }
+
