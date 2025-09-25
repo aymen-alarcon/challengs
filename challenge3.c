0291150566f345e00b45c1eb6896c595fa598320
Aymen Oumaalla <aymenoml2002@gmail.com>
2025-09-25 10:52:03 +0100

    second question added to 3rd challenge

--- challenge3.c
+++ challenge3.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 int main(void) {
+    /*
     bool digit_seen[10] = {false}; 
     bool digit_repeated[10] = {false}; 
     int digit; 
@@ -21,8 +22,42 @@ int main(void) {
 
     for (int i = 0; i < 10; i++) {
         if (digit_repeated[i]) {
-            printf("Chiffres répétés: %d ", i);
+            printf("Chiffres repetes: %d ", i);
         }
     }
     return 0;
+    */
+
+    //second question
+    bool digit_seen[10] = {false}; 
+    int digit_repeated[10] = {0}; 
+    int digit; 
+    long n;
+    int count = 0;
+
+    printf("Entrez un nombre: "); 
+    scanf("%ld", &n);
+
+    while (n > 0) 
+    { 
+        digit = n % 10;             
+        digit_repeated[digit]++;
+        n /= 10;                     
+    }
+printf("Chiffre : \t");
+    for (int i = 0; i < 10; i++) {
+        printf("%d\t", i);
+    }
+    printf("\nOccurrences : \t");
+    for (int i = 0; i < 10; i++) {
+        if (digit_repeated[i]) {
+            printf("%d \t", digit_repeated[i]);
+        }else
+        {
+            printf("0 \t");
+        }
+        
+    }
+    return 0;
+
 }
